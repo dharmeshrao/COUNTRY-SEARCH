dcdad19cac7720799d879d53120c6caf85fd7b5b
Dharmesh <yadavdydv@gmail.com>
2022-01-03 22:08:01 +0530

    up[date

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const app = require('./index')
 const connect = require('./configs/db')
-app.listen(2323,async ()=>{
+app.listen(3000,async ()=>{
     await connect()
-    console.log('listning on port 2323');
 })
