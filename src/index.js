d5da5b7852871aa4665fe57bde7fbfc3a834031c
Dharmesh <yadavdydv@gmail.com>
2022-01-04 12:59:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const countryController = require("./controllers/country.controllers");
+var cors = require("cors");
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/country", countryController);
 module.exports = app;
